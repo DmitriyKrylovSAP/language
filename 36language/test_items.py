d7280b60e73c1e47f32738d8e411327dba6df3d2
DmitriyKrylovSAP <98580187+DmitriyKrylovSAP@users.noreply.github.com>
2022-03-05 16:14:01 +0300

    Add files via upload

--- 36language/test_items.py
+++ 36language/test_items.py
@@ -1,34 +1,5 @@
-from selenium import webdriver
-import time
-import pytest
-import math
-
-
-@pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
-    yield browser
-    print("\nquit browser..")
-    browser.quit()
-
-
-@pytest.mark.parametrize('links', ["895", "896", "897", "898", "899", "903", "904", "905"])
-def test_ufo(browser, links):
-    answer = math.log(int(time.time()))
-    link = f"https://stepik.org/lesson/236{links}/step/1"
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+def test_search_busket_button(browser):
     browser.get(link)
-    browser.implicitly_wait(5)
-    browser.find_element_by_class_name("string-quiz__textarea").send_keys(str(answer))
-    browser.find_element_by_class_name("submit-submission").click()
-    well = browser.find_element_by_class_name("smart-hints__hint").text
-    assert well == "Correct!", "значения одинковые"
-
- 
-
-
-
-
-
-
-
+    assert len(browser.find_elements_by_class_name("btn-add-to-basket")) > 0,  "Кнопка не найдена"
+    browser.quit()
